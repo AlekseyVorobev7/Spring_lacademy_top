c446baf8265a7c9fb68b211e6d7512d96ad71aaf
AlekseyVorobev7 <alesha.vorobev2017@yandex.ru>
2026-05-14 21:11:38 +0300

    Update

---
+++ src/main/java/com/test/test/controller/ExchangeController.java
@@ -0,0 +1,26 @@
+package com.test.test.controller;
+
+import com.test.test.model.Exchange;
+import com.test.test.service.ExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exchanges")
+public class ExchangeController {
+    @Autowired
+    private ExchangeService exchangeService;
+
+    @GetMapping
+    public ResponseEntity<List<Exchange>> getAllEx() {
+        return ResponseEntity.ok(exchangeService.getAllExchanges());
+    }
+    @GetMapping("/Japan")
+
+
+}
--- src/main/java/com/test/test/service/ExchangeService.java
+++ src/main/java/com/test/test/service/ExchangeService.java
@@ -4,7 +4,9 @@ package com.test.test.service;
 import com.test.test.model.Exchange;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -12,4 +14,20 @@ public class ExchangeService {
     private Map<Long, Exchange> exchanges = new HashMap<>();
     private Long nextId = 1L;
 
+    public ExchangeService() {
+        exchanges.put(1L, new Exchange(1L,"ByBit", "adasd", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(2L, new Exchange(1L,"ByBit", "Japan", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(3L, new Exchange(1L,"ByBit", "Japaadasdn", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(4L, new Exchange(1L,"ByBit", "Japasdasdan", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(5L, new Exchange(1L,"ByBit", "asdasd", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(6L, new Exchange(1L,"ByBit", "asdasd", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(7L, new Exchange(1L,"ByBit", "Japasdasasan", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(8L, new Exchange(1L,"ByBit", "Japasdasdan", "https://b.b", "Nice", true, 2009L));
+        exchanges.put(9L, new Exchange(1L,"ByBit", "asd", "https://b.b", "Nice", true, 2009L));
+    }
+    // http://localhost:8080/api/exchanges/Japan
+    public List<Exchange> getAllExchanges() {
+        return new ArrayList<>(exchanges.values());
+    }
+    public
 }
